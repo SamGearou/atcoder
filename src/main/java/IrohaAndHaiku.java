41e0338dedcaa0d692966905f257e35e1ec32544
SamGearou <sgearou@atlassian.com>
2021-12-29 18:23:18 -0600

    iroha and haiku (ABC) edition

---
+++ src/main/java/IrohaAndHaiku.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+// Iroha and Haiku
+public class IrohaAndHaiku {
+
+    public static void main(String[] args) throws IOException {
+        IrohaAndHaiku.solution();
+    }
+
+    private static void solution() throws IOException {
+        MyScanner scanner = new MyScanner();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(a, map.getOrDefault(a, 0) + 1);
+        map.put(b, map.getOrDefault(b, 0) + 1);
+        map.put(c, map.getOrDefault(c, 0) + 1);
+        PrintWriter printWriter = new PrintWriter(System.out);
+        if (map.getOrDefault(5, 0) == 2 && map.getOrDefault(7, 0) == 1) {
+            printWriter.println("YES");
+        } else {
+            printWriter.println("NO");
+        }
+        printWriter.close();
+    }
+
+    public static class MyScanner {
+        private BufferedReader bufferedReader;
+        private StringTokenizer st;
+
+        public MyScanner() {
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(bufferedReader.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+}
