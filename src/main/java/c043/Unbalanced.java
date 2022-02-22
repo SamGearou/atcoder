a4734a9d511e7f7d16aa0dd3ecf417a0c0d4884f
SamGearou <sgearou@atlassian.com>
2022-02-22 08:35:16 -0800

    Unbalanced from ABC 043

--- src/main/java/c043/Unbalanced.java
+++ src/main/java/c043/Unbalanced.java
@@ -4,33 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
-// Be Together
-public class BeTogether {
+// Unbalanced
+public class Unbalanced {
 
     private static void solution() throws IOException {
         MyScanner scanner = new MyScanner();
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+        String str = scanner.next();
+        HashMap<Character, Integer> prevInd = new HashMap<>();
+        boolean foundAns = false;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (prevInd.containsKey(c) && (prevInd.get(c) == i - 1 || prevInd.get(c) == i - 2)) {
+                PrintWriter printWriter = new PrintWriter(System.out);
+                printWriter.println((prevInd.get(c) + 1) + " " + (i + 1));
+                printWriter.close();
+                foundAns = true;
+                break;
+            }
+            prevInd.put(c, i);
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = -100; i <= 100; i++) {
-            min = Math.min(min, findCost(i, arr));
+        if (!foundAns) {
+            PrintWriter printWriter = new PrintWriter(System.out);
+            printWriter.println("-1 -1");
+            printWriter.close();
         }
-        PrintWriter printWriter = new PrintWriter(System.out);
-        printWriter.println(min);
-        printWriter.close();
-    }
-
-    private static int findCost(int num, int[] arr) {
-        int totalCost = 0;
-        for (int x : arr) {
-            totalCost += (Math.abs(x - num) * Math.abs(x - num));
-        }
-        return totalCost;
     }
 
     public static class MyScanner {
